5745358d4eb1c4ec6022e5c9f6f7fe2619a94e95
tapopadma <tapopadmatripathy1995@gmail.com>
2025-08-06 12:27:14 +0530

    concurrent stock exchange

--- src/main/java/StockExchange.java
+++ src/main/java/StockExchange.java
@@ -5,12 +5,15 @@ import java.util.stream.*;
 import org.apache.commons.lang3.tuple.*;
 import org.junit.*;
 import org.junit.runner.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+import java.util.concurrent.locks.*;
 
 
 public class StockExchange {
 
-	private static int TRADE_ID = 1;
-	private static int ORDER_ID = 1;
+	private final AtomicInteger TRADE_ID = new AtomicInteger();
+	private final AtomicInteger ORDER_ID = new AtomicInteger();
 
 	enum OrderType {
 		BUY,SELL;
@@ -48,6 +51,7 @@ public class StockExchange {
 
 	class SEEngine {
 		TreeSet<Order> sellOrders, buyOrders;
+		final Lock lock = new ReentrantLock();
 
 		SEEngine() {
 			sellOrders = new TreeSet<>((s1,s2)->s1.price==s2.price?Integer.compare(s1.orderId,s2.orderId):Integer.compare(s1.price, s2.price));
@@ -55,33 +59,39 @@ public class StockExchange {
 		}
 
 		List<Trade> processOrder(Order order) {
-			List<Trade> trades = new ArrayList<>();
-			if(order.orderType.equals(OrderType.BUY)) {
-				while(!sellOrders.isEmpty() && sellOrders.first().price <= order.price && order.quantity > 0) {
-					Order sellOrder = sellOrders.first();
-					int tradeQuantity = Math.min(order.quantity, sellOrder.quantity);
-					int tradePrice = sellOrder.price;
-					sellOrder.quantity = sellOrder.quantity - tradeQuantity;
-					order.quantity = order.quantity - tradeQuantity;
-					if(sellOrder.quantity==0)sellOrders.pollFirst();
-					trades.add(new Trade(TRADE_ID++,sellOrder.orderId,order.orderId,tradeQuantity,tradePrice,System.currentTimeMillis()));
+			List<Trade> trades;
+			lock.lock();
+			try {
+				trades = new ArrayList<>();
+				if(order.orderType.equals(OrderType.BUY)) {
+					while(!sellOrders.isEmpty() && sellOrders.first().price <= order.price && order.quantity > 0) {
+						Order sellOrder = sellOrders.first();
+						int tradeQuantity = Math.min(order.quantity, sellOrder.quantity);
+						int tradePrice = sellOrder.price;
+						sellOrder.quantity = sellOrder.quantity - tradeQuantity;
+						order.quantity = order.quantity - tradeQuantity;
+						if(sellOrder.quantity==0)sellOrders.pollFirst();
+						trades.add(new Trade(TRADE_ID.incrementAndGet(),sellOrder.orderId,order.orderId,tradeQuantity,tradePrice,System.currentTimeMillis()));
+					}
+					if(order.quantity > 0){
+						buyOrders.add(order);
+					}
+				} else {
+					while(!buyOrders.isEmpty() && buyOrders.first().price >= order.price && order.quantity > 0) {
+						Order buyOrder = buyOrders.first();
+						int tradeQuantity = Math.min(order.quantity, buyOrder.quantity);
+						int tradePrice = order.price;
+						buyOrder.quantity = buyOrder.quantity - tradeQuantity;
+						order.quantity = order.quantity - tradeQuantity;
+						if(buyOrder.quantity==0)buyOrders.pollFirst();
+						trades.add(new Trade(TRADE_ID.incrementAndGet(),order.orderId,order.orderId,tradeQuantity,tradePrice,System.currentTimeMillis()));
+					}
+					if(order.quantity > 0){
+						sellOrders.add(order);
+					}
 				}
-				if(order.quantity > 0){
-					buyOrders.add(order);
-				}
-			} else {
-				while(!buyOrders.isEmpty() && buyOrders.first().price >= order.price && order.quantity > 0) {
-					Order buyOrder = buyOrders.first();
-					int tradeQuantity = Math.min(order.quantity, buyOrder.quantity);
-					int tradePrice = order.price;
-					buyOrder.quantity = buyOrder.quantity - tradeQuantity;
-					order.quantity = order.quantity - tradeQuantity;
-					if(buyOrder.quantity==0)buyOrders.pollFirst();
-					trades.add(new Trade(TRADE_ID++,order.orderId,order.orderId,tradeQuantity,tradePrice,System.currentTimeMillis()));
-				}
-				if(order.quantity > 0){
-					sellOrders.add(order);
-				}
+			} finally {
+				lock.unlock();
 			}
 			return trades;
 		}
@@ -91,12 +101,35 @@ public class StockExchange {
 	@Test
 	public void testSinglePurchaseSell() throws Exception{
 		SEEngine se = new SEEngine();
-		se.processOrder(new Order(ORDER_ID++,OrderType.BUY,10,5));
-		se.processOrder(new Order(ORDER_ID++,OrderType.SELL,9,3));
+		se.processOrder(new Order(ORDER_ID.incrementAndGet(),OrderType.BUY,10,5));
+		se.processOrder(new Order(ORDER_ID.incrementAndGet(),OrderType.SELL,9,3));
 		assertTrue(se.sellOrders.isEmpty());
 		assertEquals(se.buyOrders.size(),1);
 		assertEquals(se.buyOrders.first().price,10);
 		assertEquals(se.buyOrders.first().quantity,2);
+	}
+
+	@Test
+	public void testConcurrentOrders() throws Exception{
+		SEEngine se = new SEEngine();
+		ExecutorService server = Executors.newFixedThreadPool(50);//50 worker threads
+		for(int i=0;i<100000;++i){
+			int q=i+1;
+			server.submit(()-> {
+				se.processOrder(new Order(ORDER_ID.incrementAndGet(),OrderType.SELL,10,q));
+			});
+		}
+		for(int i=0;i<200000;++i){
+			int q = (i+1)/2;
+			server.submit(()-> {
+				se.processOrder(new Order(ORDER_ID.incrementAndGet(),OrderType.BUY,20,q));
+			});
+		}
+		server.shutdown();
+		try {
+			server.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
+		} catch(InterruptedException e){}
+		assertTrue(se.sellOrders.isEmpty());
 	}
 
 	public static void main(String[] args){
